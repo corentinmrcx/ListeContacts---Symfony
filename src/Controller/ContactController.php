284bd70e57f2e30178a0583c1a4597641031cb05
Corentin Marcoux <corentin.marcoux@etudiant.univ-reims.fr>
2024-10-23 11:52:51 +0200

    Détails d'un contact,version naïve

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
@@ -15,5 +16,17 @@ class ContactController extends AbstractController
         $contacts = $contactRepository->findBy([], ['lastname' => 'ASC', 'firstname' => 'ASC']);
 
         return $this->render('contact/index.html.twig', ['contacts' => $contacts]);
+    }
+
+    #[Route('/contact/{contactId}', requirements: ['contactId' => '\d+'] )]
+    public function show(int $contactId, ContactRepository $contactRepository): Response
+    {
+        $contact = $contactRepository->find($contactId);
+
+        if (!$contact){
+            throw new NotFoundHttpException('Contact not found');
+        }
+
+        return $this->render('contact/show.html.twig', ['contactId' => $contactId , 'contact' => $contact]);
     }
 }
