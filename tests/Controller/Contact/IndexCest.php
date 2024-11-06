baec914901f90c71a088a0fb3444f9b0ff56433d
Corentin Marcoux <corentin.marcoux@etudiant.univ-reims.fr>
2024-11-06 10:58:33 +0100

    Ecriture d'un test qui vérifie que le lien du contact renvoie la bonne route

--- tests/Controller/Contact/IndexCest.php
+++ tests/Controller/Contact/IndexCest.php
@@ -14,4 +14,10 @@ class IndexCest
         $I->see('Liste des contacts', 'h1');
         $I->seeNumberOfElements('ul > li', 195);
     }
+
+    public function testRouteContact(ControllerTester $I): void{
+        $I->amOnPage('/contact');
+        $I->click('ul > li > a[href]');
+        $I->seeCurrentRouteIs('app_contact_show');
+    }
 }
