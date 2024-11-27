f4686d9c2b0140a2c7f5a7c07ea33a2ab3c7f02d
marc0237 <corentin.marcoux@etudiant.univ-reims.fr>
2024-11-27 21:33:26 +0100

    Affecter une catégorie à 90% des contacts générés

--- src/DataFixtures/ContactFixtures.php
+++ src/DataFixtures/ContactFixtures.php
@@ -2,16 +2,28 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\CategoryFactory;
 use App\Factory\ContactFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
+use function Zenstruck\Foundry\faker;
+
 class ContactFixtures extends Fixture implements DependentFixtureInterface
 {
-    public function load(ObjectManager $manager): void
+    public function load(ObjectManager $manager)
     {
-        ContactFactory::createMany(150);
+        ContactFactory::createMany(150, function () {
+            if (faker()->boolean(90)) {
+                return [
+                    'category' => CategoryFactory::random(),
+                ];
+            }
+            return [
+                'category' => null,
+            ];
+        });
     }
 
     public function getDependencies(): array
