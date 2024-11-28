bda7d260542c902f15c1a34574cf46d7c7e22b3a
marc0237 <corentin.marcoux@etudiant.univ-reims.fr>
2024-11-28 16:45:20 +0100

    Critère de tri sur la collection des contacts

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -6,6 +6,7 @@ use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\OrderBy;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
 class Category
@@ -19,6 +20,7 @@ class Category
     private ?string $name = null;
 
     #[ORM\OneToMany(targetEntity: Contact::class, mappedBy: 'category')]
+    #[OrderBy(['lastname' => 'ASC', 'firstname' => 'ASC'])]
     private Collection $contacts;
 
     public function __construct()
