3de5b754b5853df1741fa7c3b8f5adb5b965cb94
marc0237 <corentin.marcoux@etudiant.univ-reims.fr>
2024-11-28 16:03:58 +0100

    Action "show" qui liste les contacts d'une catégorie

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use App\Repository\CategoryRepository;
+use App\Repository\ContactRepository;
+use Symfony\Bridge\Doctrine\ArgumentResolver\EntityValueResolver;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,6 +18,15 @@ class CategoryController extends AbstractController
         $category = $categoryRepository->findBy([], ['name' => 'ASC']);
         return $this->render('category/index.html.twig', [
             'allCategory' => $category,
+        ]);
+    }
+
+    #[Route('/category/{id}', name: 'app_category_show')]
+    public function show(?Category $category, ContactRepository $contactRepository): Response{
+
+        $contacts = $contactRepository->findBy(['category' => $category]);
+        return $this->render('category/show.html.twig', [
+            'contacts' => $contacts
         ]);
     }
 }
