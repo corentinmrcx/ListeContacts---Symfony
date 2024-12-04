6b0d9e8a1f82f3a252716e2f88fdeab4c629e50d
Corentin Marcoux <corentin.marcoux@etudiant.univ-reims.fr>
2024-12-04 10:26:22 +0100

    Listage des catégories avec le nombre de contacts qu'elles contiennent

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -14,7 +14,14 @@ class CategoryController extends AbstractController
     #[Route('/category', name: 'app_category')]
     public function index(CategoryRepository $categoryRepository): Response
     {
-        $category = $categoryRepository->findBy([], ['name' => 'ASC']);
+        $qb = $categoryRepository->createQueryBuilder('c')
+                                    ->leftJoin('c.contacts', 'co')
+                                    ->select('c as category')
+                                    ->addSelect('COUNT(co) AS count')
+                                    ->groupBy('c.id')
+                                    ->orderBy('c.name', 'ASC');
+
+        $category = $qb->getQuery()->getResult();
 
         return $this->render('category/index.html.twig', [
             'allCategory' => $category,
