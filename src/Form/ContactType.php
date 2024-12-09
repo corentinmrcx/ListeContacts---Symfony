26d619036cdd01f3eed54a9bfd6f4163b150c503
marc0237 <corentin.marcoux@etudiant.univ-reims.fr>
2024-12-09 20:41:25 +0100

    Modification de la définition du champ "category" pour récupérer les données triées

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Category;
 use App\Entity\Contact;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -21,6 +22,10 @@ class ContactType extends AbstractType
             ->add('category', EntityType::class, [
                 'class' => Category::class,
                 'choice_label' => 'name',
+                'query_builder' => function (EntityRepository $entityRepository) {
+                    return $entityRepository->createQueryBuilder('c')
+                        ->orderBy('c.name', 'ASC');
+                },
             ])
         ;
     }
