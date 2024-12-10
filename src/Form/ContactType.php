754ef504e275d6804f8b48521d7365f0971e40ef
marc0237 <corentin.marcoux@etudiant.univ-reims.fr>
2024-12-10 09:53:03 +0100

    Modification types des champs : email et phone

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -7,6 +7,8 @@ use App\Entity\Contact;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,8 +19,8 @@ class ContactType extends AbstractType
         $builder
             ->add('firstname')
             ->add('lastname')
-            ->add('email')
-            ->add('phone')
+            ->add('email', EmailType::class)
+            ->add('phone', TelType::class)
             ->add('category', EntityType::class, [
                 'class' => Category::class,
                 'choice_label' => 'name',
