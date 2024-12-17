1be4b8e2da22127c4660fce760adef0b2ffed95b
marc0237 <corentin.marcoux@etudiant.univ-reims.fr>
2024-12-17 17:40:15 +0100

    Création d'une nouvelle forge de données avec modification de la méthode "defaults" pour fixer les valeurs du contact

---
+++ src/Factory/UserFactory.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+use Zenstruck\Foundry\Persistence\Proxy;
+use Zenstruck\Foundry\Persistence\ProxyRepositoryDecorator;
+
+/**
+ * @extends PersistentProxyObjectFactory<User>
+ *
+ * @method        User|Proxy                              create(array|callable $attributes = [])
+ * @method static User|Proxy                              createOne(array $attributes = [])
+ * @method static User|Proxy                              find(object|array|mixed $criteria)
+ * @method static User|Proxy                              findOrCreate(array $attributes)
+ * @method static User|Proxy                              first(string $sortedField = 'id')
+ * @method static User|Proxy                              last(string $sortedField = 'id')
+ * @method static User|Proxy                              random(array $attributes = [])
+ * @method static User|Proxy                              randomOrCreate(array $attributes = [])
+ * @method static UserRepository|ProxyRepositoryDecorator repository()
+ * @method static User[]|Proxy[]                          all()
+ * @method static User[]|Proxy[]                          createMany(int $number, array|callable $attributes = [])
+ * @method static User[]|Proxy[]                          createSequence(iterable|callable $sequence)
+ * @method static User[]|Proxy[]                          findBy(array $attributes)
+ * @method static User[]|Proxy[]                          randomRange(int $min, int $max, array $attributes = [])
+ * @method static User[]|Proxy[]                          randomSet(int $number, array $attributes = [])
+ */
+final class UserFactory extends PersistentProxyObjectFactory
+{
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        parent::__construct();
+
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public static function class(): string
+    {
+        return User::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+            'email' => self::faker()->unique()->numerify('user-###@example.com'),
+            'firstname' => self::faker()->firstName(),
+            'lastname' => self::faker()->lastName(),
+            'password' => 'test',
+            'roles' => [],
+        ];
+    }
+
+    protected function initialize(): static
+    {
+        return $this
+            ->afterInstantiate(function (User $user) {
+                $user->setPassword($this->passwordHasher->hashPassword($user, $user->getPassword()));
+            });
+    }
+}
