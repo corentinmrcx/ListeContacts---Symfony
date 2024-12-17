7104817931d37ab93790760742899bdf86754188
marc0237 <corentin.marcoux@etudiant.univ-reims.fr>
2024-12-17 17:46:37 +0100

    Correction du nom du champ "roles" et de son type dans UserFixtures.php

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -10,8 +10,8 @@ class UserFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        UserFactory::createOne(['firstname' => 'Tony', 'lastname' => 'Stark', 'email' => 'root@example.com', 'role' => 'ROLE_ADMIN']);
-        UserFactory::createOne(['firstname' => 'Peter', 'lastname' => 'Parker', 'email' => 'user@example.com', 'role' => 'ROLE_USER']);
+        UserFactory::createOne(['firstname' => 'Tony', 'lastname' => 'Stark', 'email' => 'root@example.com', 'roles' => ['ROLE_ADMIN']]);
+        UserFactory::createOne(['firstname' => 'Peter', 'lastname' => 'Parker', 'email' => 'user@example.com', 'roles' => ['ROLE_USER']]);
         UserFactory::createMany(10);
 
         $manager->flush();
