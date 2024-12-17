5569af60423746e549331c834ee1f35b2a6b8d1f
marc0237 <corentin.marcoux@etudiant.univ-reims.fr>
2024-12-17 18:49:01 +0100

    Mise à jour des tests pour vérifier le fonctionnement de l'authentification

--- tests/Controller/Contact/UpdateCest.php
+++ tests/Controller/Contact/UpdateCest.php
@@ -3,12 +3,18 @@
 namespace App\Tests\Controller\Contact;
 
 use App\Factory\ContactFactory;
+use App\Factory\UserFactory;
 use App\Tests\Support\ControllerTester;
 
 class UpdateCest
 {
-    public function form(ControllerTester $I): void
+    public function formShowsContactDataBeforeUpdating(ControllerTester $I): void
     {
+        $user = UserFactory::createOne(['roles' => ['ROLE_ADMIN']]);
+        $admin = $user->_real();
+
+        $I->amLoggedInAs($admin);
+
         ContactFactory::createOne([
             'firstname' => 'Homer',
             'lastname' => 'Simpson',
@@ -18,5 +24,30 @@ class UpdateCest
 
         $I->seeInTitle('Édition de Simpson, Homer');
         $I->see('Édition de Simpson, Homer', 'h1');
+    }
+
+    public function accessIsRestrictedToAuthenticatedUsers(ControllerTester $I): void
+    {
+        ContactFactory::createOne([
+            'firstname' => 'Homer',
+            'lastname' => 'Simpson',
+        ]);
+
+        $I->amOnPage('/contact/1/update');
+        $I->seeCurrentUrlEquals('/login');
+    }
+
+    public function accessIsRestrictedToAdminUsers(ControllerTester $I): void
+    {
+        ContactFactory::createOne([
+            'firstname' => 'Homer',
+            'lastname' => 'Simpson',
+        ]);
+
+        $user = UserFactory::createOne(['roles' => ['ROLE_USER']]);
+        $regularUser = $user->_real();
+        $I->amLoggedInAs($regularUser);
+        $I->amOnPage('/contact/1/update');
+        $I->seeResponseCodeIs(403);
     }
 }
