ccda455b0f7b5420f32efa79af2bffd2902363bb
marc0237 <corentin.marcoux@etudiant.univ-reims.fr>
2024-12-17 18:14:52 +0100

    Autorisation de l'accès au CategoryController seulement aux utilisateurs connectés

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -8,7 +8,9 @@ use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('IS_AUTHENTICATED_REMEMBERED')]
 class CategoryController extends AbstractController
 {
     #[Route('/category', name: 'app_category')]
