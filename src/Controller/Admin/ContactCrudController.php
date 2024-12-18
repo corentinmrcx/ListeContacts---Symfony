8147d9bdf9ea473e5a87987e2ff412345f4155c6
Corentin Marcoux <corentin.marcoux@etudiant.univ-reims.fr>
2024-12-18 11:23:04 +0100

    Masquage de l'identifiant des contact dans le formulaire

--- src/Controller/Admin/ContactCrudController.php
+++ src/Controller/Admin/ContactCrudController.php
@@ -20,7 +20,8 @@ class ContactCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
+            IdField::new('id')
+                ->hideOnForm(),
             TextField::new('firstname'),
             TextField::new('lastname'),
             TextField::new('email'),
