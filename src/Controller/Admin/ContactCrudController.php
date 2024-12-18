ce1136927d61781c95129d0552ac59cf65f8a234
Corentin Marcoux <corentin.marcoux@etudiant.univ-reims.fr>
2024-12-18 11:20:53 +0100

    Configuration des champs pour l'entité "Contact" : - Utilisation du "choice_label" pour afficher le nom de la catégorie - Trie sur la liste de choix des catégories

--- src/Controller/Admin/ContactCrudController.php
+++ src/Controller/Admin/ContactCrudController.php
@@ -3,7 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Contact;
+use Doctrine\ORM\EntityRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -15,14 +17,22 @@ class ContactCrudController extends AbstractCrudController
         return Contact::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('firstname'),
+            TextField::new('lastname'),
+            TextField::new('email'),
+            TextField::new('phone'),
+            AssociationField::new('category')
+                ->setFormTypeOptions(['choice_label' => 'name',
+                    'query_builder' => function (EntityRepository $entityRepository) {
+                        return $entityRepository->createQueryBuilder('c')
+                        ->orderBy('c.name', 'ASC');
+                    },
+                ]),
+            TextEditorField::new('id'),
         ];
     }
-    */
 }
