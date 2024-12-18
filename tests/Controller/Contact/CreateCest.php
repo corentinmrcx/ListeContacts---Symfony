392ba513d81107e87af3aa1e2869effc11859d06
Corentin Marcoux <corentin.marcoux@etudiant.univ-reims.fr>
2024-12-18 10:17:00 +0100

    Installation de EasyAdmin

--- tests/Controller/Contact/CreateCest.php
+++ tests/Controller/Contact/CreateCest.php
@@ -2,15 +2,28 @@
 
 namespace App\Tests\Controller\Contact;
 
+use App\Factory\UserFactory;
 use App\Tests\Support\ControllerTester;
 
 class CreateCest
 {
-    public function form(ControllerTester $I): void
+    public function formShowsContactCreationPage(ControllerTester $I): void
     {
+        $user = UserFactory::createOne(['roles' => ['ROLE_ADMIN']]);
+        $admin = $user->_real();
+
+        $I->amLoggedInAs($admin);
+
         $I->amOnPage('/contact/create');
 
         $I->seeInTitle("Création d'un nouveau contact");
         $I->see("Création d'un nouveau contact", 'h1');
+        $I->seeElement('form');
+    }
+
+    public function accessIsRestrictedToAuthenticatedUsers(ControllerTester $I): void
+    {
+        $I->amOnPage('/contact/create');
+        $I->seeCurrentUrlEquals('/login');
     }
 }
