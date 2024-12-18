bb8732737d9c98a19897d98dab936099e17917e5
marc0237 <corentin.marcoux@etudiant.univ-reims.fr>
2024-12-18 17:35:59 +0100

    Surchagement des méthodes "persistEntity" et "updateEntity"

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,14 +3,23 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserCrudController extends AbstractCrudController
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public static function getEntityFqcn(): string
     {
         return User::class;
@@ -31,5 +40,25 @@ class UserCrudController extends AbstractCrudController
                 ->setRequired(false)
                 ->setFormTypeOptions(['mapped' => false, 'empty_data' => '', 'attr' => ['autocomplete' => 'new-password']]),
         ];
+    }
+
+    public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $this->setUserPassword($entityInstance);
+        parent::updateEntity($entityManager, $entityInstance);
+    }
+
+    private function setUserPassword($entityInstance): void
+    {
+        $password = $this->getContext()->getRequest()->get('User')['password'];
+        if ('' !== $password) {
+            $entityInstance->setPassword($this->passwordHasher->hashPassword($entityInstance, $password));
+        }
+    }
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $this->setUserPassword($entityInstance);
+        parent::persistEntity($entityManager, $entityInstance);
     }
 }
